741bfe6127a3f6255386f55244099766919a0cf1
Meyer <travis.meyer@accenture.com>
2018-05-23 14:06:09 -0500

    Changed Category to work

--- server/src/main/java/com/tgm/recipe/data/Category.java
+++ server/src/main/java/com/tgm/recipe/data/Category.java
@@ -2,22 +2,24 @@ package com.tgm.recipe.data;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
-import lombok.Setter;
-import lombok.ToString;
-
 @Entity
-@Getter @Setter
-@NoArgsConstructor
-@ToString @EqualsAndHashCode
 public class Category {
-	@Id @GeneratedValue
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
 	
-	private @NonNull String name;
+	private String name;
+	
+	public Category(){}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
 }
--- server/src/main/java/com/tgm/recipe/repo/CategoryRepository.java
+++ server/src/main/java/com/tgm/recipe/repo/CategoryRepository.java
@@ -1,9 +1,10 @@
 package com.tgm.recipe.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.tgm.recipe.data.Category;
-
+@RepositoryRestResource
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
 }
